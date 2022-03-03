b514619e95936ce60eac1e194bcb2359b683f398
Sebastian Kargl <kargl121@gmail.com>
2022-03-03 15:24:26 +0100

    intial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { readFile } = require('fs')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  readFile('./home.html', 'utf-8', (err, html) => {
+      if(err){
+          res.send("service currenty unavailable");
+      }
+
+      res.send(html);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
