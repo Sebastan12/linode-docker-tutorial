dd907b528ac0472c74cb5738dad9ad463221a32a
Sebastian Kargl <kargl121@gmail.com>
2022-03-03 15:36:28 +0100

    added dockerfile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { readFile } = require('fs')
 const app = express()
-const port = 3000
+const port =  process.env.PORT|| 3000
 
 app.get('/', (req, res) => {
   readFile('./home.html', 'utf-8', (err, html) => {
